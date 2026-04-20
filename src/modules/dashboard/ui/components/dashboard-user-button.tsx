cd1e22c065f68137cb7e85d4b8f48a4c2ff8160f
JeetDas5 <jeet15083011@gmail.com>
2026-04-20 19:49:14 +0530

    feat: billings

--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -70,7 +70,10 @@ export const DashboardUserButton = () => {
               <DrawerDescription>{data.user.email}</DrawerDescription>
             </DrawerHeader>
             <DrawerFooter>
-              <Button variant="outline" onClick={() => {}}>
+              <Button
+                variant="outline"
+                onClick={() => authClient.customer.portal()}
+              >
                 <CreditCardIcon className="size-4 text-black" /> Billing
               </Button>
               <Button variant="outline" onClick={onLogout}>
@@ -114,7 +117,10 @@ export const DashboardUserButton = () => {
           </div>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem className="cursor-pointer flex items-center justify-between">
+        <DropdownMenuItem
+          onClick={() => authClient.customer.portal()}
+          className="cursor-pointer flex items-center justify-between"
+        >
           Billings
           <CreditCardIcon className="size-4" />
         </DropdownMenuItem>
--- src/modules/premium/ui/views/upgrade-view.tsx
+++ src/modules/premium/ui/views/upgrade-view.tsx
@@ -90,7 +90,6 @@ export const UpgradeView = () => {
           })}
         </div>
       </div>
-      ge
     </div>
   );
 };
